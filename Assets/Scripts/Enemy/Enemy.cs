1580ac6e1b3911b490d6e72bf040ba06de10ff43
ulti <neo.ljung@gmail.com>
2026-02-17 10:20:50 +0100

    Zombie AI

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,7 +8,6 @@ public class Enemy : MonoBehaviour
     private NavMeshAgent nav;
     private Animator animator;
     private GameObject player;
-    private bool isAttacking = false;
 
     public LayerMask obstacleLayerMasks;
     public float viewDistance = 5f;
@@ -20,9 +19,7 @@ public class Enemy : MonoBehaviour
     {
         // Get component references
         nav = GetComponentInParent<NavMeshAgent>();
-        if (!nav) Debug.Log("No NavMeshAgent component found in Enemy parent object");
         animator = GetComponent<Animator>();
-        if (!animator) Debug.Log("No Animator component was found on Enemy");
     }
 
 
@@ -34,17 +31,13 @@ public class Enemy : MonoBehaviour
 
         // Get player
         player = GameObject.Find("Player");
-        if (!player) Debug.Log("No gameObject tagged 'Player' was found for Enemy.");
     }
 
     
     void Update()
     {
         // Send data to Animator
-        if(animator) animator.SetFloat("move", nav.velocity.magnitude);
-
-        // Stop updating if Player is missing or dead
-        if (!player || player.GetComponent<Health>().isDead) return;
+        // animator.SetFloat("move", nav.velocity.magnitude);
 
         // Create a Linecast between this enemy and player
         RaycastHit2D hit = Physics2D.Linecast(transform.position, player.transform.position, obstacleLayerMasks);
@@ -52,49 +45,11 @@ public class Enemy : MonoBehaviour
         // Linecast to target was succesful (did not hit anything on obstacleLayerMasks)
         if (!hit)
         {
-            // Play Attack animation if within attack range
-            if (Vector2.Distance(transform.position, player.transform.position) < attackRange)
+            if (Vector2.Distance(transform.position, player.transform.position) < viewDistance)
             {
-                // Is not already attacking
-                if(!isAttacking)
-                {
-                    // Set attack animation
-                    if(animator) animator.SetTrigger("attack");
-                    isAttacking = true;
-                }
-                // Draw a debug line from enemy to player 
-                Debug.DrawLine(transform.position, player.transform.position, Color.red);
-            }
-            // Move to target position if within view distance
-            else if (Vector2.Distance(transform.position, player.transform.position) < viewDistance)
-            {
+                Debug.DrawLine(gameObject.transform.position, player.transform.position);
                 nav.destination = player.transform.position;
-                Debug.DrawLine(transform.position, player.transform.position);
             }
         }
-        // Obstacle in the way
-        else
-        {
-            // Draw a debug line from enemy to obstacle
-            Debug.DrawLine(transform.position, hit.point);
-        }
-    }
-
-
-    // todo: Call this method on a suitable frame in attack animation
-    public void Attack()
-    {
-        if (!player) return;
-
-        // Still within attack range and player is still alive?
-        if (Vector2.Distance(transform.position, player.transform.position) < attackRange
-            && !player.GetComponent<Health>().isDead)
-        {
-            // Player takes damage
-            player.GetComponent<Health>().TakeDamage(damage);
-        }
-
-        // is not longer attacking
-        isAttacking = false;
     }
 }
