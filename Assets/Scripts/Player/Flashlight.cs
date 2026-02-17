2cc2a55ea7e2eba697412007786f8784fc373671
Exalt <esaxist@gmail.com>
2026-02-17 11:38:52 +0100

    ME, MYSELF AND I MADE FLASHLIGHT. NEO CONTRIBUTED &"%

--- Assets/Scripts/Player/Flashlight.cs
+++ Assets/Scripts/Player/Flashlight.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,9 +6,13 @@ public class Flashlight : MonoBehaviour
 {
     bool isOn = false;
     public GameObject light;
-    public Transform player;
-    public Transform  flashlight;
+    public GameObject player;
 
+    private PlayerController playerController;
+    void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -30,7 +35,27 @@ public class Flashlight : MonoBehaviour
             light.SetActive(isOn);
         }
 
-        flashlight.rotation = player.rotation;
+        if(playerController.lastDir.y > 0)
+        {
+            light.transform.rotation = Quaternion.Euler(0,0,0);
+            light.transform.position = transform.position + new Vector3(0,0.27f,0);
+        }
+        else if(playerController.lastDir.y < 0)
+        {
+            light.transform.rotation = Quaternion.Euler(0,0,180);
+            light.transform.position = transform.position + new Vector3(0,-0.25f,0);
+        }
+         else if(playerController.lastDir.x > 0)
+        {
+            light.transform.rotation = Quaternion.Euler(0,0,270);
+            light.transform.position = transform.position + new Vector3(0.1f,0,0);
+        }
+        else if(playerController.lastDir.x < 0)
+        {
+            light.transform.rotation = Quaternion.Euler(0,0,90);
+            light.transform.position = transform.position + new Vector3(-0.1f,0,0);
+        }
+
     }
 
     void FixedUpdate()
