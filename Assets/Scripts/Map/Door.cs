918b646bfccfd0000d3c063975e738b3166a80b8
Exalt <esaxist@gmail.com>
2026-03-24 23:30:27 +0100

    DID some changes door script

--- Assets/Scripts/Map/Door.cs
+++ Assets/Scripts/Map/Door.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using NUnit.Framework;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
@@ -8,9 +10,10 @@ using UnityEngine.UIElements;
 public class Door : MonoBehaviour
 {
     public bool isOpen = false;
-    public bool inrange = false;
+    public bool needKey = true;
     public BoxCollider2D trigger;
     public BoxCollider2D restriction;
+    public TextMeshProUGUI informingMessage;
     private SpriteRenderer spriteRenderer;
 
     private void Awake() {
@@ -21,14 +24,45 @@ public class Door : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
             {PlayerController playerController = collision.GetComponent<PlayerController>();
-            if (Keyboard.current.eKey.isPressed && playerController.key > 0)
+            if (Keyboard.current.eKey.isPressed && playerController.key > 0 && needKey == true)
             {
                 isOpen = true;
                 playerController.key--;
                 restriction.enabled = false;
                 trigger.enabled = false;
                 spriteRenderer.enabled = false;
+                informingMessage.text = "Door is now open";
+                StartCoroutine(DoSomethingAfterDelay(3F));
+            }
+            else if(Keyboard.current.eKey.isPressed && needKey == false)
+            {
+                isOpen = true;
+                restriction.enabled = false; 
+                trigger.enabled = false;
+                informingMessage.text = "Door is now open";
+                StartCoroutine(DoSomethingAfterDelay(3F));
+            }
+            else if(Keyboard.current.eKey.isPressed && playerController.key <= 0)
+            {
+                informingMessage.text = "Door is locked, you need a key";
+                StartCoroutine(DoSomethingAfterDelay(3f)); 
             }
         } 
     }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            informingMessage.text = "";
+        }
+    }
+
+      IEnumerator DoSomethingAfterDelay(float timer)
+    {
+        yield return new WaitForSeconds(timer); 
+        informingMessage.text = "";
+    }
 }
+
+
+
