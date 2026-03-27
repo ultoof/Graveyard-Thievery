d079e8b9fe63623eb445ade5961f04fa29f68b68
Kotttbullenn <Viggolindberg05@gmail.com>
2026-03-27 09:32:50 +0100

    Three more scripts commented

--- Assets/Scripts/Camera/HeartsAnimator.cs
+++ Assets/Scripts/Camera/HeartsAnimator.cs
@@ -3,22 +3,23 @@ using UnityEngine.UI;
 
 public class HeartsAnimator : MonoBehaviour
 {
+    // Properties 
     public Image[] hearts;
     public Health healthScript;
     private int nrOfHearts;
 
     void Start()
     {
-        nrOfHearts = hearts.Length;
+        nrOfHearts = hearts.Length; // How many hearts you start with 
     }
 
     void Update()
     {
         float frame = (float)healthScript.health / (float)healthScript.maxHealth;
-        DisplayHearts(Mathf.RoundToInt(frame * nrOfHearts));
+        DisplayHearts(Mathf.RoundToInt(frame * nrOfHearts)); // It will remove hearts when taking damage 
     }
 
-    void DisplayHearts(int count)
+    void DisplayHearts(int count) // It shows the hearts
     {
         for (int i = 0; i < hearts.Length; i++)
         {
--- Assets/Scripts/Camera/LayerController.cs
+++ Assets/Scripts/Camera/LayerController.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class LayerController : MonoBehaviour
 {
+    // Properties 
     private SpriteRenderer spriteRenderer;
 
     void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>(); // GEts component 
     }
 
     void FixedUpdate()
     {
-        spriteRenderer.sortingOrder = -(int)(transform.position.y * 100);
+        spriteRenderer.sortingOrder = -(int)(transform.position.y * 100); // Moves the players layer so it always is above or belove stuff like bushes and walls.
     }
 }
