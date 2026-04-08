1b5d68496849b8fcd7fcb7656a9488551272b81d
Zzrowpro <esaxist@gmail.com>
2026-04-08 08:50:12 +0200

    A few edits to some scipts, started implementing an item tracker

--- Assets/Scripts/Player/Taser.cs
+++ Assets/Scripts/Player/Taser.cs
@@ -6,29 +6,20 @@ public class Taser : MonoBehaviour
     public GameObject taserProjectilePrefab;
     public int ammo = 5;
     public float shootingRate = 0.5f;
-    public bool canStun = true;
     public GameObject shootVFX;
-    public bool hasBeenUsed = false;
     private ParticleSystem shootParticle;
 
     private void Awake() {
         shootParticle = shootVFX.GetComponent<ParticleSystem>(); // Gets the particle system 
     }
 
-    void Start()
-    {
-        if (DataManager.instance != null)
-        canStun = DataManager.instance.canStun;
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T) && canStun == true) // Checks if the player is pressing down T
+        if (Input.GetKeyDown(KeyCode.T) && DataManager.instance.canStun == true) // Checks if the player is pressing down T
         {
             if (ammo > 0) // Checks if the player has ammo 
             {
-                hasBeenUsed = true;
                 ammo--; // Removes one bullet
                 shootParticle.Play(); // Spawns Particle 
                 shootVFX.GetComponent<AudioSource>().Play(); // Plays the sound
--- Assets/Scripts/Testing/ShowButtonAfterEvent.cs
+++ Assets/Scripts/Testing/ShowButtonAfterEvent.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Test : MonoBehaviour
+public class ShowButtonAfterEvent : MonoBehaviour
 {
     public GameObject ExitCutsceneButton;
     
