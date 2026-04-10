a28cf8c1b226efd3dbbf5a5b116bf06fd945ee54
Zzrowpro <esaxist@gmail.com>
2026-04-10 07:41:47 +0200

    The cog is ugly

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -7,16 +7,15 @@ using Unity.VisualScripting;
 public class Health : MonoBehaviour
 {
     // Properties 
-    public int health = 3;
     public int maxHealth = 3;
     public bool isDead = false;
     public GameObject gameOverPanel;
 
     public void TakeDamage(int damage) // Makes the player take damage 
     {
-        health = math.clamp(health - damage, 0, maxHealth); 
+        DataManager.instance.health = math.clamp(DataManager.instance.health - damage, 0, maxHealth); 
 
-        if (health <= 0) // If the player is dead 
+        if (DataManager.instance.health <= 0) // If the player is dead 
         {
             gameOverPanel.SetActive(true); // Spawns the Game over screen 
             Debug.Log("H");
